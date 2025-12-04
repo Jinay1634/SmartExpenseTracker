5468091fd32ad7f10b88eab7e19f8efba501f8df
jinay1634 <pateljinaygomesh@gmail.com>
2025-12-04 07:34:33 -0500

    Init

--- app/src/main/java/week11/st679303/finalproject/viewmodel/MainViewModel.kt
+++ app/src/main/java/week11/st679303/finalproject/viewmodel/MainViewModel.kt
@@ -13,6 +13,7 @@ import week7.st991662903.midpractice.utils.UiState
 
 class MainViewModel: ViewModel() {
     public val auth = FirebaseAuth.getInstance()
+
     private val repo = BillRepository()
     private val _uiState = MutableStateFlow<UiState>(UiState.Loading)
     val uiState: StateFlow<UiState> = _uiState
